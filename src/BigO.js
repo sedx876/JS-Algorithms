4df10b142df8b44eee71fcdca839cdfaf98b6cbd
sedx876 <swatkins876@gmail.com>
2020-10-25 08:57:26 -0400

    big o and array notes

---
+++ src/BigO.js
@@ -0,0 +1,18 @@
+//Quantifiable way to measure how efficient certain data
+//structures are at different tasks we might ask of it
+//Searching through, modifying and accessing
+
+// A way to basically Score a data structure based on 4 criteria
+// The most common functions you might want from a data structure
+//   Accessing Elements 
+//   Searching for an element 
+//   Inserting an element 
+//   Deleting an element
+
+// A Time Complexity Equation works by inserting the size of the Data 
+// set as an integer n, and returning the number of operations 
+// that need to be conducted by the computer before the function can finish
+
+// N = the size of the data set (amount of elements contained within
+//   the data structure)
+
---
+++ src/arrays.js
@@ -0,0 +1,20 @@
+// An array is a list of similar values 
+// Can be used to store anything 
+// Store values of the same type 
+// Every item in the list of data is reffered to as an element 
+
+
+// the collective total of elements is an array 
+// An array usually has 3 attributes
+// A name: used to reference and interact with it
+
+// A type: what type of info is stored or will be stored within the array 
+//         has to hold all the same type of info
+
+// A size: is a set integer that is fixed upon creation of array
+//         represents the total number of elements that are able to 
+//         be stored within the array
+//         cannot be changed
+
+
+// An array with an array at each index is known as a 2-dimensional array 
